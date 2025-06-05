0ae36e70cad1fc033ded416a467f91e953a7bd5a
Joshik Raj <joshikrajjojo@gmail.com>
2025-06-05 16:09:07 +0530

    added button for excel

--- college_selection.js
+++ college_selection.js
@@ -9,3 +9,17 @@
     
 // Goes to the previous page
   }
+
+function downloadExcel() {
+  const downloadUrl = `${window.env.BASE_URL}/exports`;
+
+  // Create a temporary anchor element
+  const a = document.createElement('a');
+  a.href = downloadUrl;
+  a.download = 'student_export.xlsx'; // Suggested filename
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+
